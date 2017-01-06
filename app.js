2c5a04541e5d393ecc5d96981c26c61300723823
Tomas <tomasholderness@gmail.com>
2017-01-06 15:23:07 -0500

    Working multi-language version

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config({silent:true});
 const app = new Telegraf(process.env.BOT_TOKEN);
 
 // GRASP card
-var options = {
+const options = {
   host: 'https://data-dev.petabencana.id',
   path: '/cards',
   method: 'POST',
@@ -20,18 +20,28 @@ var options = {
   }
 }
 
-app.command('start', (ctx) => {
-  ctx.reply("Hi! Saya Bencana Bot.\n/banjir - laporan banjir ");
-});
+const langs = {
+  '/banjir': 'id',
+  '/flood': 'en'
+}
 
-app.command(['flood', 'banjir'], (ctx) => {
+const replies = {
+  'id': 'Hi! Laporan menggunakan link ini, terima kasih',
+  'en': 'Hi! Report using this link, thanks'
+}
 
-  console.log('Received flood report request');
+var get_card = function(ctx, callback){
+
+  // Get language
+  var language = process.env.DEFAULT_LANG;
+  if (ctx.update.message.text in langs){
+    var language = langs[ctx.update.message.text]
+  }
 
   var card_request = {
     "username": ctx.from.id.toString(),
     "network": "telegram",
-    "language": process.env.DEFAULT_LANG
+    "language": language
   }
 
   // Get a card
@@ -45,11 +55,30 @@ app.command(['flood', 'banjir'], (ctx) => {
 
   }, function(error, response, body){
     if (!error && response.statusCode === 200){
-      console.log('Received card, reply to user');
-      ctx.reply('Hi! Laporkan menggunakan link ini '+ process.env.CARD_PATH + body.cardId + ' Terima kasih.');
+      callback(null, replies[language] + '\n' + process.env.CARD_PATH + body.cardId);
     }
     else {
-      console.log('Error getting card: ' + JSON.stringify(error) + JSON.stringify(response));
+      var err = 'Error getting card: ' + JSON.stringify(error) + JSON.stringify(response);
+      callback(err, null);
+    }
+  });
+}
+
+
+app.command('start', (ctx) => {
+  ctx.reply("Hi! Saya Bencana Bot.\n/banjir - laporan banjir ");
+});
+
+app.command(['flood', 'banjir'], (ctx) => {
+  console.log('Received flood report request');
+
+  get_card(ctx, function(err, response){
+    if (!err){
+      console.log('Received card, reply to user');
+      ctx.reply(response);
+    }
+    else {
+      console.log('Error getting card: ' + err);
     }
   });
 });
